b0d0607fa3bd15bfde85a1026d2fef46acf8f51a
Shareef Alam <alshareef4039@gmail.com>
2025-04-02 20:09:54 +0530

    code initialize

---
+++ src/test/java/com/db/optimize/DatabaseOptimizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.optimize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseOptimizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
