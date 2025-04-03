b87b705d3743bdb06ed24d1d3876b7e43990b822
Shareef Alam <alshareef4039@gmail.com>
2025-04-03 14:01:12 +0530

    code refactor

---
+++ src/main/java/com/db/optimize/config/MysqlConfig.java
@@ -0,0 +1,37 @@
+package com.db.optimize.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class MysqlConfig {
+
+    @Value("${mysql.datasource.url}")
+    private String dbUrl;
+
+    @Value("${mysql.datasource.username}")
+    private String dbUsername;
+
+    @Value("${mysql.datasource.password}")
+    private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+
+        dataSource.setMaximumPoolSize(20);
+        dataSource.setMinimumIdle(5);
+        dataSource.setConnectionTimeout(30000);
+        dataSource.setIdleTimeout(600000);
+
+        return dataSource;
+    }
+}
